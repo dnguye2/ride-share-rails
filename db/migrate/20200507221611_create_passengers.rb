54e02a7c822dd833188daf358773b64a26f2e2cb
dnguye2 <dnguyen1093@gmail.com>
2020-05-07 15:57:18 -0700

    database successfully seeded

--- db/migrate/20200507221611_create_passengers.rb
+++ db/migrate/20200507221611_create_passengers.rb
@@ -1,7 +1,8 @@
 class CreatePassengers < ActiveRecord::Migration[6.0]
   def change
     create_table :passengers do |t|
-
+      t.string :name
+      t.bigint :phone_num
       t.timestamps
     end
   end
--- db/migrate/20200507221620_create_trips.rb
+++ db/migrate/20200507221620_create_trips.rb
@@ -1,7 +1,11 @@
 class CreateTrips < ActiveRecord::Migration[6.0]
   def change
     create_table :trips do |t|
-
+      t.bigint :driver_id
+      t.bigint :passenger_id
+      t.datetime :date
+      t.bigint :rating
+      t.bigint :cost
       t.timestamps
     end
   end
