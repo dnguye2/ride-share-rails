785f2cca7cd2b383fc758842c651a2ee07c4f9d2
mulhoo <oliviamulholland94@gmail.com>
2020-05-08 11:01:38 -0700

    return driver method in model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,12 @@ class Driver < ApplicationRecord
   has_many :trips
   validates :name, presence: true, format: {with: /[a-zA-Z]/} 
   validates :vin, presence: true
+
+  def self.find_driver
+    @drivers = Driver.all
+
+    @drivers.each do |driver|
+      return driver if driver.status == "available"
+    end
+  end
 end
