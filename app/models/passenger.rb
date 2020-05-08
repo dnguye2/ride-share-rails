1a049f4a78391efc1c565d81a0af60240edec6ca
mulhoo <oliviamulholland94@gmail.com>
2020-05-08 12:59:27 -0700

    trip and passenger models

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,17 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def expenses
+    passenger_trips = self.trips
+    total_spent = 0
+
+    return nil if passenger_trips.nil?
+
+    passenger_trips.each do |trip|
+      total_spent += trip.cost
+    end
+    
+    return total_spent
+  end
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,12 @@
 class Trip < ApplicationRecord
   belongs_to :passenger
   belongs_to :driver
+
+  def self.find_driver
+    @drivers = Driver.all
+
+    @drivers.each do |driver|
+      return driver if driver.status == "available"
+    end
+  end
 end
