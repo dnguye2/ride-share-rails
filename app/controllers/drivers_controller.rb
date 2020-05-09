a1900c8f183ff3ecfa569be3e88feb1867c35e43
dnguye2 <dnguyen1093@gmail.com>
2020-05-08 17:26:36 -0700

    skeleton for controller actions added

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,29 @@
 class DriversController < ApplicationController
+  describe "index" do
+    
+  end
+
+  describe "show" do
+    
+  end
+
+  describe "new" do
+    
+  end
+
+  describe "create" do
+    
+  end
+
+  describe "edit" do
+    
+  end
+
+  describe "update" do
+    
+  end
+
+  describe "destroy" do
+    
+  end
 end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,21 @@
 class TripsController < ApplicationController
+  describe "show" do
+    
+  end
+
+  describe "create" do
+    
+  end
+
+  describe "edit" do
+    
+  end
+
+  describe "update" do
+    
+  end
+
+  describe "destroy" do
+    
+  end
 end
