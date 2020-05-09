7b7c3b2899649b563399876cf24a49b957d597f9
dnguye2 <dnguyen1093@gmail.com>
2020-05-08 20:10:34 -0700

    added migration to change phone_num type to string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_221620) do
+ActiveRecord::Schema.define(version: 2020_05_09_030748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2020_05_07_221620) do
 
   create_table "passengers", force: :cascade do |t|
     t.string "name"
-    t.bigint "phone_num"
+    t.string "phone_num"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
