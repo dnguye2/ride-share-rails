4ee899e991fb11e85f615c4c9175d123d33d0434
mulhoo <oliviamulholland94@gmail.com>
2020-05-11 12:39:23 -0700

    added tests for dripass models

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -1,16 +1,50 @@
 require "test_helper"
 
 describe Driver do
-
-  it "must have a name" do
-    new_driver = Driver.create(name: nil, vin: "234023F")
-
-    expect (new_driver.valid?).must_equal false
+  let (:new_driver) {
+    Driver.new(name: "Jake the Dog", vin: "234", available: true)
+  }
+  
+  it "can be instantiated" do
+    expect(new_driver.valid?).must_equal true
   end
 
-  it "must have a phone number" do 
-    new_driver = Driver.create(name: "X Æ A-12", vin: nil)
+  it "will have all required fields" do
+    new_driver.save
+    driver = Driver.first
 
-    expect (new_driver.valid?).must_equal false
+    [:name, :vin, :available]. each do |field|
+      expect(driver).must_respond_to field
+    end
+  end
+
+  describe "validations" do
+    it "must have a name" do
+      new_driver = Driver.create(name: nil, vin: "234023F")
+
+      expect(new_driver.valid?).must_equal false
+      expect(new_driver.errors.messages).must_include :name
+      expect(new_driver.errors.messages[:name]).must_equal ["can't be blank"]
+    end
+
+    it "must have a vin" do 
+      new_driver = Driver.create(name: "X Æ A-12", vin: nil)
+
+      expect(new_driver.valid?).must_equal false
+      expect(new_driver.errors.messages).must_include :vin
+      expect(new_driver.errors.messages[:vin]).must_equal ["can't be blank"]
+    end
+  end
+
+  describe "relationships" do
+    it "can have many trips" do
+      new_driver.save
+      driver = Driver.first
+
+      expect(driver.trips.count).must_be :>=, 0
+      driver.trips.each do |trip|
+        expect(trip).must_be_instance_of trip
+      end
+    end
   end
 end
--- test/models/passenger_test.rb
+++ test/models/passenger_test.rb
@@ -1,19 +1,38 @@
 require "test_helper"
 
 describe Passenger do
-  before do
-    test_case_1 = Passenger.new(name: 'forrest', phone_num: '24')
+
+  let (:new_passenger) {
+    Passenger.new(name: "Finn the Human", phone_num: "234")
+  }
+
+  describe "validations" do
+    it "must have a name" do
+      new_passenger = Passenger.create(name: nil, phone_num: "4257619410")
+
+      expect(new_passenger.valid?).must_equal false
+      expect(new_passenger.errors.messages).must_include :name
+      expect(new_passenger.errors.messages[:name]).must_equal ["can't be blank"]
+    end
+
+    it "must have a phone number" do 
+      new_passenger = Passenger.create(name: "X Æ A-12", phone_num: nil)
+
+      expect(new_passenger.valid?).must_equal false
+      expect(new_passenger.errors.messages).must_include :phone_num
+      expect(new_passenger.errors.messages[:phone_num]).must_equal ["can't be blank"]
+    end
   end
 
-  it "must have a name" do
-    new_passenger = Passenger.create(name: nil, phone_num: "4257619410")
+  describe "relationships" do
+    it "can have many trips" do
+      new_passenger.save
+      passenger = Passenger.first
 
-    expect (new_passenger.valid?).must_equal false
-  end
-
-  it "must have a phone number" do 
-    new_passenger = Passenger.create(name: "X Æ A-12", phone_num: nil)
-
-    expect (new_passenger.valid?).must_equal false
+      expect(passenger.trips.count).must_be :>=, 0
+      passenger.trips.each do |trip|
+        expect(trip).must_be_instance_of trip
+      end
+    end
   end
 end
