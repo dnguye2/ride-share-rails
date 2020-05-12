c87ff723e0de0a1c02f77b1bf158a544c3e6a486
dnguye2 <dnguyen1093@gmail.com>
2020-05-11 23:44:45 -0700

    actions completed

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -9,8 +9,34 @@ class TripsController < ApplicationController
     end
   end
 
+  def new
+    passenger_id = params[:passenger_id]
+    
+    @trip = Trip.new
+    
+    if passenger_id.nil?
+      @passengers = Passenger.all
+    else
+      @passengers = [Passenger.find_by(id: passenger_id)]
+    end
+  end
+
   def create 
-  
+    avail_driver = Trip.find_driver
+    avail_driver_id = avail_driver.id
+    passenger_id = params[:passenger]
+
+    @trip = Trip.new(passenger_id: passenger_id, driver_id: avail_driver_id)
+
+    if @trip.save
+      @trip.driver.available = false
+      @trip.driver.save
+      redirect_to trip_path(@trip.id)
+      return
+    else
+      redirect_to passengers_path
+      return
+    end
   end
 
   def edit 
@@ -18,16 +44,44 @@ class TripsController < ApplicationController
     @trip = Trip.find_by(id: trip_id)
 
     if @trip.nil?
-      redirect_to trips_path
+      head :not_found
       return
     end
   end
 
   def update 
-    
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      redirect_to root_path
+      return
+    elsif @trip.update(trip_params)
+      # @trip.update(rating: params[:trip][:rating])
+      @trip.driver.available = false
+      @trip.driver.save
+      redirect_to trip_path(@trip.id)
+      return
+    else
+      render :edit, status: :bad_request
+      return
+    end
   end
 
   def destroy 
-    
+    trip = Trip.find_by(id: params[:id])
+
+    if trip.nil?
+      return_to trips_path
+      return
+    else
+      trip.destroy
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :cost, :rating, :driver_id, :passenger_id)
   end
 end
